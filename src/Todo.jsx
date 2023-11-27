526dc46c97ae0e9f7154fdcdb171bff5ce33500a
Plumnet <tomomi0131@ymail.ne.jp>
2023-11-27 14:15:11 +0900

    タスクの戻す機能

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -3,8 +3,8 @@ import "./styles.css";
 
 export const Todo = () => {
   const [todoText, setTodoText] = useState("");
-  const [incompleteTodos, setIncompleteTodos] = useState(["TODOです1", "TODOです2"]);
-  const [completeTodos, setCompleteTodos] = useState(["TODOでした1", "TODOでした2"]);
+  const [incompleteTodos, setIncompleteTodos] = useState([]);
+  const [completeTodos, setCompleteTodos] = useState([]);
 
   const onChangeTodoText = (event) => setTodoText(event.target.value);
 
@@ -28,6 +28,14 @@ export const Todo = () => {
     const newCompleteTodos = [...completeTodos, incompleteTodos[index]];
     setIncompleteTodos(newIncompleteTodos);
     setCompleteTodos(newCompleteTodos);
+  };
+
+  const onClickBack = (index) => {
+    const newCompleteTodos = [...completeTodos];
+    newCompleteTodos.splice(index, 1);
+    const newIncompleteTodos = [...incompleteTodos, completeTodos[index]];
+    setCompleteTodos(newCompleteTodos);
+    setIncompleteTodos(newIncompleteTodos);
   }
 
   return (
@@ -53,11 +61,11 @@ export const Todo = () => {
     <div className="complete-area">
       <p className="title">完了のTODO</p>
       <ul>
-        {completeTodos.map((todo) => (
+        {completeTodos.map((todo, index) => (
           <li key={todo}>
             <div className="list-row">
               <p className="todo-item">{todo}</p>
-              <button>戻す</button>
+              <button onClick={() => onClickBack(index)}>戻す</button>
               </div>
           </li>
       ))}
